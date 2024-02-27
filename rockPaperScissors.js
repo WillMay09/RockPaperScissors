405af383db7805ca4b914ac890a007fe74a8d43c
WillMay09 <wmayhood@outlook.com>
2024-02-27 16:05:39 -0500

    created a convertUserInput function
    This function converts the user's string input into a numeric value for proper comparison with the computer's number

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -8,19 +8,46 @@ let computerInput = Math.floor(Math.random()*3)+1;
 
 function convertUserInput(userInput){
 
+    switch(userInput){
+
+        case "rock":
+        userInput = 1
+        return userInput;
+
+        case "paper":
+
+        userInput = 2
+        return userInput;
+
+        case "scissors":
+        userInput = 3
+        return userInput;
+
+        default:
+
+        console.log("the computer made an error");
+
+    }
+}
+
+///rock=1, paper=2, scissors=3
+function checkWin(numericUserVal, computerInput){
+
+
 
 
 }
 
 function playGame(userInput, computerInput){
+//converts user string value into a number
+let numericUserVal = convertUserInput(userInput);
 
-//let numericUserVal = convertUserInput(userInput);
 
 
 
 }
 
 
-
+playGame(userInput, computerInput);
 console.log(computerInput);
 console.log(computerInput);
