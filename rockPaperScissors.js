f8e02d84c54ce44f5ffb10db7164195c4d27d976
WillMay09 <wmayhood@outlook.com>
2024-02-27 15:24:46 -0500

    Created an html skeleton and set up javaScript variables
    An html skeleton is used for testing and the javaScript variables serve as the foundation for which the game is built on

---
+++ rockPaperScissors.js
@@ -0,0 +1,26 @@
+
+//prompts user to type rock, paper or scissor. non-case sensitive
+let userInput = prompt("Rock, Paper, or Scissors? Your Choice").toLowerCase();
+
+//stores the computer's random input of 1, 2, or 3.
+let computerInput = Math.floor(Math.random()*3)+1;
+
+
+function convertUserInput(userInput){
+
+
+
+}
+
+function playGame(userInput, computerInput){
+
+//let numericUserVal = convertUserInput(userInput);
+
+
+
+}
+
+
+
+console.log(computerInput);
+console.log(computerInput);
